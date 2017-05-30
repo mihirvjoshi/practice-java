f1e6d886967c737dd5ee03478112a5296eb58636
Mihir <Mihir@192.168.1.100>
2017-05-30 16:25:47 +0530

    included pallindrome check.

--- src/com/practice/java/datastructure/MyLinkedList.java
+++ src/com/practice/java/datastructure/MyLinkedList.java
@@ -3,11 +3,9 @@ package com.practice.java.datastructure;
 public class MyLinkedList {
 	Node first;
 	Node last;
-
 	int SIZE = 0;
 
-	public MyLinkedList() {
-	}
+	public MyLinkedList() {	}
 
 	MyLinkedList(Node first) {
 		this.first = first;
@@ -15,7 +13,6 @@ public class MyLinkedList {
 		while (currentNode.next != null) {
 			currentNode = currentNode.next;
 		}
-
 		this.last = currentNode;
 	}
 
@@ -27,7 +24,6 @@ public class MyLinkedList {
 		} else {
 			Node newNode = new Node(o);
 			Node currentNode = first;
-
 			// here it's important to check current.next for not null value
 			// because
 			// at the last node current will become null and so there will not
@@ -45,21 +41,19 @@ public class MyLinkedList {
 	private class Node {
 		Object data;
 		Node next;
-
 		Node(Object data) {
 			this.data = data;
 			this.next = null;
 		}
-
 		Node() {
 			this.next = null;
 		}
 
 	}
 
-	public void reverse() {
+	public Node reverse(Node node) {
 		Node previous = null, newFirst = null, temp = null;
-		Node current = first;
+		Node current = node;
 		// newFirst = current;
 		// current = current.next;
 		// newFirst.next = null;
@@ -70,9 +64,7 @@ public class MyLinkedList {
 			// current = current.next;
 			// temp.next = previous;
 			// newFirst = temp;
-
 			previous = current;
-
 			// #1. this step is important in the sequence here because
 			// current(memory address) is still being referred by previous in
 			// last step.
@@ -88,8 +80,8 @@ public class MyLinkedList {
 			newFirst = previous;
 			newFirst.next = temp;
 		}
-
-		first = newFirst;
+		node = newFirst;
+		return node;
 	}
 
 	/**
@@ -99,9 +91,7 @@ public class MyLinkedList {
 	 * @return
 	 */
 	public void reverseKthNodesOfList(int k) {
-
 		int count = 1;
-
 		Node current = first;
 		Node temp = null;
 		Node previous = null;
@@ -109,26 +99,20 @@ public class MyLinkedList {
 		MyLinkedList listOfSubLists = new MyLinkedList();
 
 		while (current != null) {
-
 			previous = current;
 			current = current.next;
 			temp = newFirst;
 			newFirst = previous;
 			newFirst.next = temp;
-
 			if (count == k || current == null) { // if count is 3 or list ended
 													// before count reached to 3
 				count = 0;
 				listOfSubLists.add(new MyLinkedList(newFirst));
 				newFirst = null;
 			}
-
 			++count;
 		}
-
 		MyLinkedList reversedList = mergeSubLists(listOfSubLists);
-
-		// System.out.println("merged list is =>"+reversedList.toString());
 		first = reversedList.first;
 	}
 
@@ -152,13 +136,10 @@ public class MyLinkedList {
 	}
 
 	public MyLinkedList mergeAlternateNodes(MyLinkedList anotherList) {
-
 		Node temp1 = null;
 		Node temp2 = null;
-
 		Node current = null;
 		Node newNode = null;
-		
 		Node current1 = this.first;
 		Node current2 = anotherList.first;
 
@@ -172,29 +153,45 @@ public class MyLinkedList {
 
 		while (current != null) {
 			temp1 = newNode;
-
 			if (this.SIZE > anotherList.SIZE) {
 				temp2 = current2;
 			} else {
 				temp2 = current1;
 			}
-
 			current1 = current1.next;
 			current2 = current2.next;
 			current = current.next;
-//			System.out.println(current1.data);
 			temp1.next = temp2;
 			
 			if(temp2 != null){
 				temp2.next = null;				
 			} else break;
-
 			newNode = temp1;
 		}
-
 		return null;
 	}
 
+	public boolean isPallindrome(Node node){
+		Node fast=node;
+		Node slow=node;
+		if(fast==null) return false;
+		while(fast.next!=null && fast.next.next!=null){
+			fast=fast.next.next;
+			slow=slow.next;
+		}
+		Node middle=slow;
+		Node newFirst=reverse(middle);		
+		Node current=node;
+		while(current!=null){
+			if(current.data!=newFirst.data){
+				return false;
+			}
+			current=current.next;
+			newFirst=newFirst.next;
+		}
+		return true;
+	}
+	
 	public String toString() {
 		String listAsString = "";
 		Node current = this.first;
@@ -202,46 +199,24 @@ public class MyLinkedList {
 			listAsString = listAsString + current.data + ",";
 			current = current.next;
 		}
-
 		return listAsString;
 	}
 
 	public static void main(String args[]) {
-
 		MyLinkedList myList = new MyLinkedList();
 		myList.add(10);
 		myList.add(11);
 		myList.add(12);
-		myList.add(110);
-		myList.add(111);
-		myList.add(112);
-		myList.add(113);
-		myList.add(114);
+		myList.add(13);
+		myList.add(12);
+		myList.add(11);
+		myList.add(10);
 
 		System.out.println("========================================");
-//		myList.reverse();
 		System.out.println(myList.toString());
 		System.out.println("========================================");
-		myList.reverseKthNodesOfList(4);
-		System.out.println(myList.toString());
+		System.out.println(myList.isPallindrome(myList.first));
 		System.out.println("========================================");
-		
-		//merge two lists		
-//		MyLinkedList myList1 = new MyLinkedList();
-//		myList1.add(10);
-//		myList1.add(11);
-//		myList1.add(12);
-//
-//		MyLinkedList myList2 = new MyLinkedList();
-//		myList2.add(110);
-//		myList2.add(111);
-//		myList2.add(112);
-//		myList2.add(113);
-//		myList2.add(114);
-//
-//		System.out.println("========================================");
-//		myList1.mergeAlternateNodes(myList2);
-//		System.out.println("========================================");
 		
 	}
 }
