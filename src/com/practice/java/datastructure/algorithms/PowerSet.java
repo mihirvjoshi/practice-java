5d1f51eba9576538812e92297ab0aba6c2a54a4c
Mihir <Mihir@192.168.1.100>
2017-06-12 11:22:47 +0530

    Power set

--- src/com/practice/java/datastructure/algorithms/PowerSet.java
+++ src/com/practice/java/datastructure/algorithms/PowerSet.java
@@ -1,32 +1,42 @@
 package com.practice.java.datastructure.algorithms;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class PowerSet {
 	void powerset(int arr[]){
+		Set<Set> powerSet = new HashSet<Set>();
 		System.out.println("{}");
 		for (int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i]);
+			Set<Integer> subSet = new HashSet<Integer>();
+			subSet.add(arr[i]);
+			powerSet.add(subSet);
 		}
-		powerset(arr, 0, 1, arr.length );
+		powerset(arr, 0, 1, arr.length , powerSet);
+		System.out.println(powerSet.toString());
 	}
 
-	void powerset(int arr[], int start, int power, int size){
+	void powerset(int arr[], int start, int power, int size, Set<Set> powerSet){
 		if(power==size){
 			return;
 		}
-		String mainSet=String.valueOf(arr[start]);
+		Set<Integer> currentSet=new HashSet<Integer>();
 		int cursor=start+power;
-		for(int j=start+1;j<cursor;j++){
-			mainSet=mainSet+","+arr[j];
+		for(int j=start;j<cursor;j++){
+			currentSet.add(arr[j]);
 		}
 		for(int k=cursor;k<size;k++){
-			System.out.println(mainSet+","+arr[k]);
+			Set<Integer> subSet=new HashSet<Integer>();
+			subSet.addAll(currentSet);
+			subSet.add(arr[k]);
+			powerSet.add(subSet);
 		}
 		start=start+1;
-		if(start+power>=size){
+		if(cursor>=size){
 			power=power+1;
 			start=0;
 		}
-		powerset(arr, start, power, size);
+		powerset(arr, start, power, size, powerSet);
 	}
 	
 	public static void main(String[] args) {
